a9e038b6eb1009e471dc388ccb0706c810b9c5a4
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2022-02-01 16:59:01 -0300

    Creating SplashPage

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+import "package:payflow/modules/splash/splash_page.dart";
+import "package:payflow/shared/themes/app_colors.dart";
+
+void main() {
+  runApp(MaterialApp(
+    title: "Pay Flow",
+    theme: ThemeData(primaryColor: AppColors.primary),
+    home: SplashPage(),
+  ));
+}
