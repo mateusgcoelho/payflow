c04f3e98e3b90ff9998d9161ea56dda710132dd2
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2022-02-01 19:24:07 -0300

    Finish auth shared preferences

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
 
 class LoginController {
   final AuthController _authController = AuthController();
@@ -15,8 +16,12 @@ class LoginController {
 
     try {
       final response = await _googleSignIn.signIn();
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl,
+      );
 
-      _authController.setUser(context, response);
+      _authController.setUser(context, user);
     } catch (error) {
       _authController.setUser(context, null);
     }
